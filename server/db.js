bd5df357c53939520e81f0aad0e540b9fdb51b05
Hasmik <hasmiksardaryan20@gmail.com>
2025-06-30 14:14:10 -0700

    sync

--- server/db.js
+++ server/db.js
@@ -1,13 +1,32 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const connectToDB = async() => {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/photo-editor');
-        console.log('Connected to MongoDB');
+// const connectToDB = async() => {
+//     try {
+//         await mongoose.connect('mongodb://localhost:27017/photo-editor');
+//         console.log('Connected to MongoDB');
         
-    } catch (error) {
-        console.log(error);
-    }
-}
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// export default connectToDB;
+
+
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Connected to MongoDB Atlas");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+  }
+};
 
 export default connectToDB;
